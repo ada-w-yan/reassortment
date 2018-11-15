8dc42a8f76b1d5ab4d0c9eb3d0337cac2d236f07
Ada Yan <a.yan@imperial.ac.uk>
2018-11-15 17:14:18 +0000

    added parallel cluster tools

--- R/cluster_tools.R
+++ R/cluster_tools.R
@@ -5,7 +5,7 @@
 #' @return a didehpc::queue_didehpc object
 #' @export
 setup_cluster <- function(n_cores = 1){
-    user_options <- get_user_options(n_cores)
+    user_options <- get_user_options()
     setwd(user_options$wd)
     sources <- list.files(path = "scripts", 
                             pattern = "\\.R", 
@@ -13,32 +13,29 @@ setup_cluster <- function(n_cores = 1){
                             full.names = TRUE)
     do.call(options, user_options$cluster_options)
     src <- provisionr::package_sources(local = user_options$package_dir, expire = 1e-10)
-    
     ## Setup contexts
     context::context_log_start()
     root <- "contexts"
     packages <- list(attached = c("reassortment", "ggplot2", "magrittr", "parallel"),
                      loaded = "reshape2")
     ctx <- context::context_save(packages=packages,path=root, sources=sources,package_sources=src)
-    
+    config <- didehpc::didehpc_config(cores = n_cores)
     ## Submit setup to cluster
-    obj1 <- didehpc::queue_didehpc(ctx)
+    obj1 <- didehpc::queue_didehpc(ctx, config)
     return(obj1)
 }
 
 #' specify user options for cluster
 #' 
-#' @param n_cores number of cluster cores
 #' @return a list with the working directory on the network drive; the directory
 #' in which the package code sits; and options for didehpc
 #' @export
-get_user_options <- function(n_cores = 1) {
+get_user_options <- function() {
   # wd is the working directory to run the cluster job from. 
   # This should be the user's network home drive eg. "~/net/home/reassortment"
     list(wd = "~/net/home/reassortment/",
          package_dir = "~/git_repos/reassortment/",
          cluster_options = list(didehpc.username = "ayan",
-                                didehpc.cluster = "fi--didemrchnb",
-                                cores = n_cores))
+                                didehpc.cluster = "fi--didemrchnb"))
 
 }
