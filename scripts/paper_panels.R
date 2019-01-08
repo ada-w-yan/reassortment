1eb7b28561356b2c72e6816ce9fa7f6b75c0e3ac
Ada Yan <a.yan@imperial.ac.uk>
2019-01-08 16:10:32 +0000

    add option for cell count to be constant when varying MOI, rather than virus count

--- scripts/paper_panels.R
+++ scripts/paper_panels.R
@@ -44,6 +44,50 @@ paper_panels <- function(MOI, fitness_MW = 0, fitness_WM = 1.25, mutation_prob, 
       invisible(results)
 }
 
+paper_panels_constant_n_cells <- function(MOI, mutation_prob, n_cells = 1e6, hash) {
+  set.seed(2)
+  iv <- c(95,0,0,5) #mt,mt  mt,wt  wt,mt   wt,wt
+  fitness <- c(1, 0, 1.25, 1)
+  pop_size <- round(n_cells * MOI)
+  burst_size <- 10
+  generations <- 20
+  coinfection <- TRUE
+  MOI_dependent_burst_size <- TRUE
+  choose_strain_by_fitness <- FALSE
+  one_strain_produced <- FALSE
+  n_replicates <- 10
+  run_parallel <- TRUE
+  reassort <- as.logical(reassort)
+  
+  sim_name <- paste(num2str(MOI), reassort, "constant_cells", collapse = "_")
+  dir_name <- ifelse(missing(hash),
+                     make_results_folder(sim_name),
+                     make_results_folder(sim_name, hash = hash))
+  inputs <- ls()
+  inputs <- list_vars_from_environment(inputs)
+  saveRDS(inputs, paste0(dir_name, "inputs.rds"))
+  
+  sim_func <- function(run_no)  {
+    simulate_evolution(iv, fitness, burst_size, n_cells, pop_size,
+                       generations, mutation_prob,
+                       coinfection,
+                       MOI_dependent_burst_size,
+                       choose_strain_by_fitness,
+                       one_strain_produced,
+                       reassort) %>%
+      cbind(., matrix(run_no, nrow = generations, ncol = 1, dimnames = list(NULL, "run")))
+  }
+  results <- parLapply_wrapper(run_parallel, seq_len(n_replicates), sim_func) %>%
+    do.call(rbind, .)
+  
+  # results <- readRDS(paste0(dir_name, "results.rds"))
+  g <- plot_multirun_strains(results)
+  ggsave(paste0(dir_name, "strains.pdf"), g, width = 10, height = 10, units = "cm")
+  g <- plot_multirun_segments(results)
+  ggsave(paste0(dir_name, "segments.pdf"), g, width = 10, height = 10, units = "cm")
+  invisible(results)
+}
+
 if(FALSE) {
   mutation_prob <- 2e-4
   fitness_WM <- 1.25
@@ -63,9 +107,16 @@ if(FALSE) {
                 pars_reassort_mutate,
                 pars_mutate_only)
   
-  pars$pop_size <- 1e3
+  pars$pop_size <- 1e6
   pars$hash <- get_hash()
 
   obj <- setup_cluster(n_cores = 5)
   job <- obj$enqueue_bulk(pars, paper_panels)
+  
+  diff_MOI_idx <- seq(4, 6)
+  pars <- rbind(pars_reassort_only[diff_MOI_idx,],
+                pars_reassort_mutate[diff_MOI_idx,])
+  pars$n_cells <- 1e6
+  pars$hash <- get_hash()
+  job <- obj$enqueue_bulk(pars, paper_panels_constant_n_cells)
 }
