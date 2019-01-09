70a7cecaa3c447f8ce8650e9c4d29a363f7006c7
Ada Yan <a.yan@imperial.ac.uk>
2019-01-09 10:53:54 +0000

    code to make figure showing MM proportion by MOI

--- scripts/paper_panels.R
+++ scripts/paper_panels.R
@@ -1,4 +1,5 @@
 library(magrittr)
+library(ggplot2)
 paper_panels <- function(MOI, fitness_MW = 0, fitness_WM = 1.25, mutation_prob, reassort, pop_size = 1e6, hash) {
   set.seed(2)
   iv <- c(95,0,0,5) #mt,mt  mt,wt  wt,mt   wt,wt
@@ -114,6 +115,46 @@ get_results <- function(MOI, fitness_MW = 0, fitness_WM = 1.25, mutation_prob, r
   invisible(NULL)
 }
 
+plot_end_change_MOI <- function() {
+  MOI <- 10^seq(-3, 1)
+  fitness_MW <- 0
+  fitness_WM <- 1.25
+  mutation_prob <- 2e-4
+  reassort <- TRUE
+  extract_prop_MM_by_MOI <- function(MOI) {
+    filename <- paste(num2str(c(MOI, fitness_MW, fitness_WM, mutation_prob)), collapse = "_") %>%
+    paste0("_", reassort, "/results.rds") %>%
+      paste0("results/", .)
+    results <- readRDS(filename)
+    n_runs <- max(results[,"run"])
+    n_gen <- nrow(results) / n_runs
+    results <- results[seq_len(nrow(results)) %% n_gen == 0, "MM"] %>%
+      quantile(prob = c(0.025, 0.975))
+    results
+  }
+  if(FALSE) {
+  prop_MM_by_MOI <- t(vapply(MOI, extract_prop_MM_by_MOI, double(2))) %>%
+    as.data.frame
+  prop_MM_by_MOI$MOI <- MOI
+  saveRDS(prop_MM_by_MOI, "results/prop_MM_by_MOI.rds")
+  }
+  
+  prop_MM_by_MOI <- readRDS("results/prop_MM_by_MOI.rds")
+  
+  make_label <- function(x) TeX(paste0("$10^{", x, "}$"))
+  x_labels <- sapply(log10(MOI), make_label)
+  g <- ggplot(prop_MM_by_MOI, aes(x = MOI, ymin = `2.5%`, ymax = `97.5%`)) +
+    geom_errorbar() +
+    scale_x_log10(breaks = MOI, labels = x_labels) +
+    theme_bw() +
+    coord_cartesian(ylim = c(0, 1), expand = FALSE) +
+    ylab("Proportion of MM") +
+    theme(text = element_text(size = 20))
+  ggsave("results/prop_MM_by_MOI.pdf", g, width = 10, height = 10, units = "cm")
+  saveRDS(g, "prop_MM_by_MOI_plot.rds")
+  invisible(prop_MM_by_MOI)
+}
+
 if(FALSE) {
   mutation_prob <- 2e-4
   fitness_WM <- 1.25
@@ -136,9 +177,15 @@ if(FALSE) {
   pars$pop_size <- 1e6
   pars$hash <- get_hash()
 
+  # get_results
+  pars$hash <- "2909c83"
+  pars$job_idx <- seq_len(nrow(pars))
+  apply_named_args(pars, 1, get_results)
+  
+  
   obj <- setup_cluster(n_cores = 5)
   job <- obj$enqueue_bulk(pars, paper_panels)
-  # contributable_paintedladybutterfly
+  # contributable_paintedladybutterfly (4 to go)
   
   diff_MOI_idx <- seq(4, 6)
   pars <- rbind(pars_reassort_only[diff_MOI_idx,],
@@ -147,7 +194,7 @@ if(FALSE) {
   pars$hash <- get_hash()
   pars <- pars[,c("MOI", "mutation_prob", "n_cells", "hash")]
   job <- obj$enqueue_bulk(pars, paper_panels_constant_n_cells)
-  # argumentative_grebe
+  # intracranial_lightningbug (done)
   
   pars_reassort_only <- data.frame(MOI = 1e-1,
                                    fitness_MW = 0,
@@ -165,7 +212,13 @@ if(FALSE) {
   
   obj <- setup_cluster(n_cores = 5)
   job <- obj$enqueue_bulk(pars, paper_panels)
+  # fel_motmot (done)
   
   colnames(pars)[6] <- "n_cells"
+  pars <- pars[,c("MOI", "mutation_prob", "n_cells", "hash")]
   job <- obj$enqueue_bulk(pars, paper_panels_constant_n_cells)
+  # debatable_hapuka (failed)
+  
+
+
 }
