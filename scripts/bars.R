fe1c49e18e4db4941e4979bf3f9b3906640674f1
Ada Yan <a.yan@imperial.ac.uk>
2020-07-13 08:37:12 +0100

    cleaning up

--- scripts/bars.R
+++ scripts/bars.R
@@ -1,5 +1,7 @@
 library(latex2exp)
-dir_name <- "~/overleaf/reassortment/sensitivity/figs/"
+library(tidyverse)
+dir_name <- "~/overleaf/reassortment/sensitivity/results/"
+dir_name_plot <- "~/overleaf/reassortment/sensitivity/figs/"
 fitness_WM <- c(1, 1.1, 1.5, 1.75, 2)
 reassort <- c(TRUE, FALSE)
 par_grid <- expand.grid(fitness_WM = fitness_WM, reassort = reassort)
@@ -7,7 +9,7 @@ filenames <- paste0(dir_name, "fitness_WM_", num2str(par_grid$fitness_WM), "_",
                     par_grid$reassort, "/results.rds")
 results <- lapply(filenames, readRDS)
 g <- sensitivity_plot(results, par_grid$fitness_WM, par_grid$reassort, x_lab = TeX("$f_{WM}$"))
-filename <- paste0(dir_name, "f_WM.pdf")
+filename <- paste0(dir_name_plot, "f_WM.pdf")
 ggsave(filename, g$g, width = 15, height = 10, units = "cm")
 
 fitness_MM <- c(0.8, 0.9, 1.1, 1.2)
@@ -16,7 +18,7 @@ filenames <- paste0(dir_name, "fitness_MM_", num2str(par_grid$fitness_MM), "_",
                     par_grid$reassort, "/results.rds")
 results <- lapply(filenames, readRDS)
 g <- sensitivity_plot(results, par_grid$fitness_MM, par_grid$reassort, x_lab = TeX("$f_{MM}$"))
-filename <- paste0(dir_name, "f_MM.pdf")
+filename <- paste0(dir_name_plot, "f_MM.pdf")
 ggsave(filename, g$g, width = 15, height = 10, units = "cm")
 
 mu <- c(2e-3, 2e-5)
@@ -24,8 +26,8 @@ par_grid <- expand.grid(mu = mu, reassort = reassort)
 filenames <- paste0(dir_name, "mutation_prob_", num2str(par_grid$mu), "_",
                     par_grid$reassort, "/results.rds")
 results <- lapply(filenames, readRDS)
-g <- sensitivity_plot(results, par_grid$mu, par_grid$reassort, x_lab = TeX("Mutation rate (day$^{-1}$)"))
-filename <- paste0(dir_name, "mu.pdf")
+g <- sensitivity_plot(results, par_grid$mu, par_grid$reassort, x_lab = "Mutation rate per generation")
+filename <- paste0(dir_name_plot, "mu.pdf")
 ggsave(filename, g$g, width = 15, height = 10, units = "cm")
 
 sim_name <- c("MOI_0point1", "MOI_dependent_burst_size_FALSE", "choose_strain_by_fitness_TRUE")
@@ -36,5 +38,5 @@ filenames <- paste0(dir_name, sim_name, "_",
 results <- lapply(filenames, readRDS)
 par_grid <- expand.grid(sim_name = sim_name_plot, reassort = reassort)
 g <- sensitivity_plot(results, par_grid$sim_name, par_grid$reassort, x_lab = "", rotate_x_labels = TRUE)
-filename <- paste0(dir_name, "misc.pdf")
+filename <- paste0(dir_name_plot, "misc.pdf")
 ggsave(filename, g$g, width = 15, height = 15, units = "cm")
